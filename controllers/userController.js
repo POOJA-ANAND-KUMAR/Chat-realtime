0e29a2164c93dafa6d2ab6c66128412b0b6008f6
POOJA-ANAND-KUMAR <49919979+POOJA-ANAND-KUMAR@users.noreply.github.com>
2020-09-25 09:46:50 -0700

    Create userController.js

---
+++ controllers/userController.js
@@ -0,0 +1,6 @@
+exports.register= async(req,res) => {
+    const {name ,email,password} = req.body;
+
+    const emailRegex= /@gmail.com|@yahoo.com|hotmail.com|@live.com/
+};
+exports.login= async(req,res) => {};
